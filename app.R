fdb4447ccde1fc6da459610c25e180cc6646b104
Kelly Hondula <khondula@sesynclap26.sesync.org>
2018-03-23 20:02:34 -0400

    added download button

--- app.R
+++ app.R
@@ -5,25 +5,36 @@
 
 library(shiny)
 library(shinydashboard)
+library(RPostgreSQL)
+library(DT)
+library(magrittr)
+
+# source('global.R')
+variablenames <- dbGetQuery(db, "SELECT variablecode FROM odm2.variables")
 
 ui <- dashboardPage(
   dashboardHeader(title = "ODM2 database"),
   dashboardSidebar(
+    tags$style(".skin-blue .sidebar a { color: #444; }"),
     sidebarMenu(
       menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
       menuItem("Widgets", tabName = "widgets", icon = icon("th"))
   ),
   selectizeInput(inputId = "select_variable", label = "choose variable",
-                 choices = variablenames)),
+                 choices = variablenames, selected = "methaneDissolved"),
+  downloadButton("downloadData", "Download")
+  
+  ),
   dashboardBody(
     fluidRow(
-      box(dataTableOutput("table1")))
+     
+      box(dataTableOutput("datatable1")))
   )
 )
 
 server <- function(input, output) { 
   
-  output$table1 <- renderDataTable({
+  table1_data <- reactive({
     sql <- "SELECT mrv.datavalue, mrv.valuedatetime, sf.samplingfeaturecode, r.featureactionid, v.variablecode, u.unitsname
   FROM odm2.measurementresultvalues mrv, odm2.results r, odm2.variables v, odm2.units u, odm2.samplingfeatures sf, odm2.featureactions fa
     WHERE r.variableid = v.variableid 
@@ -31,15 +42,27 @@ server <- function(input, output) {
     AND fa.samplingfeatureid = sf.samplingfeatureid
     AND r.unitsid = u.unitsid
     AND mrv.resultid = r.resultid 
-    AND r.sampledmediumcv = 'soil'
-    AND v.variablenamecv = ?variablenamecv"
+    AND variablecode = ?variablecode"
     
-    sql <- sqlInterpolate(ANSI(), sql, variablenamecv = input$select_variable)
+    sql <- sqlInterpolate(ANSI(), sql, variablecode = input$select_variable)
     # sql <- gsub("\n", "", sql)
     
     dbGetQuery(db, sql)
     
   })
+
+    output$datatable1 <- renderDataTable({ table1_data() }, 
+                                         options = list(dom = 'plt'))
+  
+  # Downloadable csv of selected dataset ----
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste0(input$select_variable, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(table1_data(), file, row.names = FALSE)
+    }
+  )
 
   
   }
--- global.R
+++ global.R
@@ -2,10 +2,9 @@ library(RPostgreSQL)
 library(DT)
 library(magrittr)
 
+password = scan(".pgpass", what = "")
 db <- dbConnect(PostgreSQL(), 
                 host = "sesync-postgis01.research.sesync.org",
                 dbname = "choptank", 
                 user = "palmergroup",
                 password = password)
-
-variablenames <- dbGetQuery(db, "SELECT variablenamecv FROM odm2.variables")
